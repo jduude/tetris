4e435c74abe9a7a2547571d65ab07a8cbe5d450a
Jukka Ylitalo <juylitalo@gmail.com>
2026-04-04 12:32:37 +0300

    format document

--- src/Board.ts
+++ src/Board.ts
@@ -1,18 +1,21 @@
 export class Board {
-  width:number;
-  height:number;
+  width: number;
+  height: number;
   state: string[][];
 
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
-    this.state= Array(this.height).fill(null)
-      .map(() => Array(this.width).fill(null)
-        .map(() => ".")
-      )  
+    this.state = Array(this.height)
+      .fill(null)
+      .map(() =>
+        Array(this.width)
+          .fill(null)
+          .map(() => ".")
+      );
   }
 
   toString() {
-    return  this.state.map(row => row.join("")).join("\n") + "\n";
+    return this.state.map((row) => row.join("")).join("\n") + "\n";
   }
 }
