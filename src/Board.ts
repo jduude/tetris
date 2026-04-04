bd79a6eb611f279e8db1ef946a8d85a0aa2990b4
Jukka Ylitalo <juylitalo@gmail.com>
2026-04-04 12:45:16 +0300

    add check for at most one block may be falling at a time

--- src/Board.ts
+++ src/Board.ts
@@ -1,21 +1,27 @@
 export class Board {
   width: number;
   height: number;
+  initialState: string[][];
   state: string[][];
 
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
-    this.state = Array(this.height)
+    this.initialState = Array(this.height)
       .fill(null)
       .map(() =>
         Array(this.width)
           .fill(null)
           .map(() => ".")
       );
+    this.state = this.initialState.slice().map((row) => row.slice());
   }
 
   drop(block: string) {
+    const existingFallingBlock = this.state.find((row) => row.find((cell) => cell !== "."));
+    if (existingFallingBlock) {
+      throw new Error("already falling");
+    }
     this.state[0][1] = block;
   }
 
