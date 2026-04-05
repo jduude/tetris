9dd43682f8ebdf2e6190cc99ab9f94db36d7e8b5
Jukka Ylitalo <juylitalo@gmail.com>
2026-04-05 11:49:47 +0300

    I shape rotation

--- test/RotatingTetrominoes.test.ts
+++ test/RotatingTetrominoes.test.ts
@@ -76,7 +76,7 @@ describe("The I shape", () => {
     );
   });
 
-  test.skip("can be rotated left/counter-clockwise", () => {
+  test("can be rotated left/counter-clockwise", () => {
     expect(shape.rotateLeft().toString()).to.equalShape(
       `..I..
        ..I..
@@ -86,7 +86,7 @@ describe("The I shape", () => {
     );
   });
 
-  test.skip("has 2 distinct orientations", () => {
+  test("has 2 distinct orientations", () => {
     expect(distinctOrientations(shape).size).to.equal(2);
   });
 });
